2b47310c91f048ed104f21544fdec99ffb67085a
Harshil <88352766+Harshil1502@users.noreply.github.com>
2022-02-22 13:30:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(function () {
     // function to calculate the distance in metres between two lat/long pairs on Earth
     // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
     // Aren't those cool variable names? Yah gotta love JavaScript
-    function calcDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+    function calcDistanceBetweenPoints(position.coords.latitude, position.coords.longitude, lat2, lon2) {
         var toRadians = function (num) {
             return num * Math.PI / 180;
         }
